c66739df83034dc833bb4d7a3fa9bf777e2a4e6b
xuweijie <xuweijie@didi.com>
2020-02-09 16:56:43 +0800

    update

--- mybatis-016/src/main/java/com/jay/Main.java
+++ mybatis-016/src/main/java/com/jay/Main.java
@@ -9,6 +9,8 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author xuweijie
@@ -17,22 +19,114 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        test01();
+        // test01();
+        // test02();
+        // test03();
+        // test04();
+        // test05();
+        // test06();
+        // test07();
+        // test08();
+        test09();
     }
 
     private static void test01() throws IOException {
-        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
-        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder
-                .build(Resources.getResourceAsReader("mybatis-config.xml"), "development");
-        // environment这个参数可以省略，因为 mybatis-config.xml 的<environments>标签指定了默认环境为development
-
-        // 以上是样板代码
-        // 以下是「业务逻辑」
-        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+        try (SqlSession sqlSession = getSqlSession()) {
             UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
             User user = userMapper.findById(1L);
             log.info("{}", user);
         }
+    }
+
+    private static void test02() throws IOException {
+        try (SqlSession sqlSession = getSqlSession()) {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            User user = userMapper.findByNameAndPasswordV1("letian", "123");
+            log.info("{}", user);
+        }
+    }
+
+    private static void test03() throws IOException {
+        try (SqlSession sqlSession = getSqlSession()) {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            User user = userMapper.findByNameAndPasswordV2("letian", "123");
+            log.info("{}", user);
+        }
+    }
+
+    private static void test04() throws IOException {
+        try (SqlSession sqlSession = getSqlSession()) {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            Map<String, Object> data = new HashMap<>();
+            data.put("username", "letian");
+            data.put("password", "123");
+            User user = userMapper.findByNameAndPasswordV3(data);
+            log.info("{}", user);
+        }
+    }
+
+    private static void test05() throws IOException {
+        try (SqlSession sqlSession = getSqlSession()) {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            Map<String, Object> data = new HashMap<>();
+            data.put("username", "letian");
+            User user = userMapper.findByNameAndPasswordV4(data, "123");
+            log.info("{}", user);
+        }
+    }
+
+    private static void test06() throws IOException {
+        try (SqlSession sqlSession = getSqlSession()) {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            Map<String, Object> data = new HashMap<>();
+            data.put("username", "letian");
+            User user = userMapper.findByNameAndPasswordV5(data, "123");
+            log.info("{}", user);
+        }
+    }
+
+    private static void test07() throws IOException {
+        try (SqlSession sqlSession = getSqlSession()) {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            User user = new User();
+            user.setName("letian");
+            user.setPassword("123");
+            user = userMapper.findByNameAndPasswordV6(user);
+            log.info("{}", user);
+        }
+    }
+
+    private static void test08() throws IOException {
+        try (SqlSession sqlSession = getSqlSession()) {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            User user = new User();
+            user.setName("letian");
+            user.setPassword("123");
+            user = userMapper.findByNameAndPasswordV7(user);
+            log.info("{}", user);
+        }
+    }
+
+    private static void test09() throws IOException {
+        try (SqlSession sqlSession = getSqlSession()) {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+            User user = new User();
+            user.setName("zhangsan");
+            user.setEmail("zhangsan@qq.com");
+            user.setPassword("456");
+
+            int result = userMapper.insertUser(user);
+            sqlSession.commit();
+            log.info("result: {}", result);
+        }
+    }
+
+    private static SqlSession getSqlSession() throws IOException {
+        SqlSessionFactory sessionFactory;
+        sessionFactory = new SqlSessionFactoryBuilder()
+                .build(Resources.getResourceAsReader("mybatis-config.xml"));
+        return sessionFactory.openSession();
     }
 
 }
--- mybatis-016/src/main/java/com/jay/mapper/UserMapper.java
+++ mybatis-016/src/main/java/com/jay/mapper/UserMapper.java
@@ -1,6 +1,9 @@
 package com.jay.mapper;
 
 import com.jay.bean.User;
+import org.apache.ibatis.annotations.*;
+
+import java.util.Map;
 
 /**
  * @author xuweijie
@@ -8,11 +11,48 @@ import com.jay.bean.User;
 public interface UserMapper {
 
     /**
-     * 根据id查询用户信息
+     * 根据id查询用户
      *
      * @param id
      * @return
      */
+    @Select("select * from user where id = #{id}")
     User findById(Long id);
+
+    @Select("select * from user where name = #{param1} and password = #{param2}")
+    @Results({ // 结果映射
+            @Result(id = true, property = "id", column = "id"),
+            @Result(property = "name", column = "name"),
+            @Result(property = "email", column = "email"),
+            @Result(property = "password", column = "password")
+    })
+    User findByNameAndPasswordV1(String name, String password);
+
+    User findByNameAndPasswordV2(@Param("username") String name, @Param("password") String password);
+
+    @Select("select * from user where name = #{username} and password = #{password}")
+    User findByNameAndPasswordV3(Map<String, Object> data);
+
+    @Select("select * from user where name = #{param1.username} and password = #{param2};")
+    User findByNameAndPasswordV4(Map<String, Object> data, String password);
+
+    @Select("select * from user where name = #{data.username} and password = #{password};")
+    User findByNameAndPasswordV5(@Param("data") Map<String, Object> data, @Param("password") String password);
+
+    @Select("select * from user where name = #{name} and password = ${password}")
+    User findByNameAndPasswordV6(User user);
+
+    @Select("select * from blog_db.user where name=#{user.name} and password=#{user.password}")
+    User findByNameAndPasswordV7(@Param("user") User user);
+
+    /**
+     * 插入用户
+     *
+     * @param user
+     * @return 影响的行数
+     */
+    @Insert("insert into user (name, email, password) values (#{name}, #{email}, #{password} );")
+    @Options(useGeneratedKeys = true, keyProperty = "id") // 自动生成主键值，赋值给id字段
+    int insertUser(User user);
 
 }
